90857769ed43844672e8b48acab6b08e7328d2d5
akhileshsugee <akhilesh.kumar@sugee.io>
2025-08-07 17:55:28 +0530

    okay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-}
+  loginUser: string = '';
+
+  constructor(
+    private router: Router,
+  ) {
+    const user = localStorage.getItem('authtoken');
+    if (!user) {
+      this.router.navigate(['login']);
+    }
+  }
+
+}      
